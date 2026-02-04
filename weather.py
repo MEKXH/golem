c19e5fdccd09efa80ccc1d38ed7058df06d04ae2
MEKXH <2408131017@qq.com>
2026-02-05 03:42:49 +0800

    chore: delete weather.py

--- weather.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, json
-url = 'https://api.open-meteo.com/v1/forecast?latitude=22.5431&longitude=114.0579&current_weather=true&timezone=Asia/Shanghai'
-data = json.loads(urllib.request.urlopen(url).read().decode())
-w = data['current_weather']
-print(f"深圳天气: {w['temperature']}°C, 风速 {w['windspeed']}km/h")
